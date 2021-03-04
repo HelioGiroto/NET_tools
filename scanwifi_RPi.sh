f19e3604ab86caa3fab96ba189ee94a09037474d
Hélio Giroto <h@h>
2021-03-04 15:17:17 -0300

    Adidcionado script para scanWiFi para PC - Ubuntu.

--- scanwifi_RPi.sh
+++ scanwifi_RPi.sh
@@ -18,36 +18,26 @@ tput sgr0
 echo " "
 
 # Criando os principais arquivos com o resultado da varredura:
-
-date > wifis_regiao.txt
-
 # para RPi:
-# sudo iw wlan0 scan >> wifis_regiao.txt
+date > wifis_regiao.txt
+sudo iw wlan0 scan >> wifis_regiao.txt
+
 # Para PC:
-sudo iwlist wlp3s0 scan >> wifis_regiao.txt
+# sudo iwlist wlp3s0 scan | grep -i signal
+# sudo iwlist wlp3s0 scan | grep -i ESSID
 # sendo que wlp3s0 acima, foi obtido com: ls -d /sys/class/net/w*
 
 
 # Cria arquivos com os campos separados de cada um dos dados capturados acima:
-# Para RPi:
-#cat wifis_regiao.txt | grep 'SSID' | cut -d':' -f2 > ssid.wifi
-#cat wifis_regiao.txt | grep 'signal' | cut -d':' -f2 > sinal.wifi
-
-
-# Para PC:
-# captura o nome das redes:
-grep 'SSID' wifis_regiao.txt | cut -d':' -f2 > ssid.wifi
-grep -i 'signal' wifis_regiao.txt | cut -d':' -f2 | cut -d'=' -f3 > sinal.wifi
-
+cat wifis_regiao.txt | grep 'SSID' | cut -d':' -f2 > ssid.wifi
+cat wifis_regiao.txt | grep 'signal' | cut -d':' -f2 > sinal.wifi
 > barras.wifi
 
 
 # O laço abaixo cria as "barrinhas" gráficas para mostrar ao usuário quais são os roteadores com o sinal mais forte da região:
 for item in $(cat sinal.wifi)
 do
-	freq=$(echo $item | cut -d' ' -f1)	
-	#freq=$(echo $item | cut -d' ' -f1 | sed '/ //g')   # para RPi	
-	# o nro de barras corresponde à qtde negativa - 100 (para ter o nro de qto falta para 100), pq qto menor o neg é melhor o sinal.
+	freq=$(echo $item | cut -d'.' -f1 | sed 's/ //g')	
 	barras=$(( (100 + $freq) /2 ))
 	seq -s= $barras | tr -d '[:digit:]' >> barras.wifi
 	
